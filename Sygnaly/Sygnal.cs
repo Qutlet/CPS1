cea790af580d268e92691483e5e9568858ba0905
Qutlet <maciej.bigos@onet.com.pl>
2021-03-21 15:01:59 +0100

    ttttt

---
+++ Sygnaly/Sygnal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPS1.Sygnaly
+{
+    public class Sygnal
+    {
+        public Random Random = new Random();
+
+        #region Wspl
+
+        public double Amplituda { get; set; } //A
+        public double CzasPoczatkowy { get; set; } //Ts
+        public double CzasKoncowy { get; set; } //Tf
+        public double OkresPodstawowy { get; set; } //T
+        public double Prawdopodobienstwo { get; set; } //P
+        //public double Amplituda { get; set; }
+        #endregion
+
+       public double SzumJednostajny(double czas = 0)
+        {
+            return Random.NextDouble() * 2 * Amplituda - Amplituda;
+        }
+
+        public double SzumGaussowski(double czas)
+        {
+            double w1 = 1.0 - Random.NextDouble();
+            double w2 = 1.0 - Random.NextDouble();
+            return Math.Sqrt(-2.0 * Math.Log(w1)) * Math.Sin(2.0 * Math.PI * w2) * Amplituda / 3;
+        }
+
+        public double SygnalSinusoidalny(double czas)
+        {
+            return Amplituda * Math.Sin((2 * Math.PI / OkresPodstawowy) * (czas - CzasPoczatkowy));
+        }
+    }
+}
---
+++ Widok/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace CPS1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(18, 22);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(107, 22);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 468);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
